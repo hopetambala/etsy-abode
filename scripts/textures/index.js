1874c0570174eedc897a786747882a81a9d4d7d9
Hope Tambala <hopetambala@gmail.com>
2025-05-14 02:25:13 -0400

    add environment!

--- scripts/textures/index.js
+++ scripts/textures/index.js
@@ -1,12 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const scene = document.querySelector("a-scene");
-
-  // Assets
-  const assets = document.createElement("a-assets");
-
-  // const boxTexture = document.createElement("img");
-  // boxTexture.id = "boxTexture";
-  // boxTexture.src = "https://i.imgur.com/mYmmbrp.jpg";
+  const assets = document.querySelector("a-assets");
 
   const groundTexture = document.createElement("img");
   groundTexture.id = "groundTexture";
@@ -14,8 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const skyTexture = document.createElement("img");
   skyTexture.id = "skyTexture";
-  skyTexture.src = "https://cdn.aframe.io/a-painter/images/sky.jpg";
+  skyTexture.src = "assets/environment/sky.jpg";
 
-  assets.append(boxTexture, groundTexture, skyTexture);
-  scene.appendChild(assets);
+  assets.append(groundTexture, skyTexture);
 });
