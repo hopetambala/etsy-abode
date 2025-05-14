6a84b98b1df567e36aa199e7c1f33ca41aa0f65a
Hope Tambala <hopetambala@gmail.com>
2025-05-14 03:34:31 -0400

    update position

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # etsy-abode
 EtsyAbode is a next-gen shopping experience—an immersive, creator-driven marketplace for statement furniture and home essentials. Step into virtual showrooms crafted by independent makers. Whether you're exploring a VR loft filled with artisan dining tables or placing a sculpted oak credenza in your living room, EtsyAbode bridges story and space, craftsmanship and context.
 
+You can find the [glitch to remix here](https://glitch.com/edit/#!/etsy-vr).
+
 ## How to run
 
 Run the following in the root folder of this project to start your own server
--- scripts/environment/index.js
+++ scripts/environment/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   sky.setAttribute("src", "#skyTexture");
   sky.setAttribute("theta-length", "90");
   sky.setAttribute("width", "2048");
-  sky.setAttribute("scale", "-1.09 1.18 0.82");
+  sky.setAttribute("scale", "3 1.18 3");
   scene.appendChild(sky);
 
   // Ground plane
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   plane.setAttribute("rotation", "-90 0 0");
   plane.setAttribute("height", "100");
   plane.setAttribute("width", "100");
+  plane.setAttribute("scale", "2 2 1");
   // plane.setAttribute("color", "#7BC8A4");
   scene.appendChild(plane);
 
